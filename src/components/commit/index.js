b81703e120edf8a58340430da032f590f6324b19
Ibrahim Islam <ibrahim.islam@live.com>
2018-12-19 00:49:25 +0700

    partial implementation commit component

--- src/components/commit/index.js
+++ src/components/commit/index.js
@@ -5,39 +5,59 @@ import { Breadcrumb,
     ListGroup,
     ListGroupItem,
     ListGroupItemHeading,
-    ListGroupItemText
+    ListGroupItemText,
+    Input
  } from 'reactstrap'
+ import { Link } from 'react-router-dom'
+
+// const CommitItem = (props) =  (
+//     <ListGroupItem>
+//         <ListGroupItemHeading>
+//             List group item heading
+//         </ListGroupItemHeading>
+//         <ListGroupItemText className="list-text">
+//             <label className="list-text-left">Some text............................................</label>
+//             <label className="list-text-right">Date</label>
+//         </ListGroupItemText>
+//     </ListGroupItem>
+// )
 
 class Commit extends Component {
+
     render() {
         return (
             <div className="gh-table-body">
                 <div className="container">
-                    <Breadcrumb>
-                        <BreadcrumbItem><a href="#">Ibrahim-Islam</a></BreadcrumbItem>
-                        <BreadcrumbItem><a href="#">cli</a></BreadcrumbItem>
-                        <BreadcrumbItem active>Commits - master</BreadcrumbItem>
-                    </Breadcrumb>
-                    <ListGroup>
-                        <ListGroupItem>
-                            <ListGroupItemHeading>
-                                List group item heading
-                            </ListGroupItemHeading>
-                            <ListGroupItemText className="list-text">
-                                <div class="list-text-left">Some text............................................</div>
-                                <div class="list-text-right">Date</div>
-                            </ListGroupItemText>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <ListGroupItemHeading>
-                                List group item heading
-                            </ListGroupItemHeading>
-                            <ListGroupItemText className="list-text">
-                                <div class="list-text-left">Some text............................................</div>
-                                <div class="list-text-right">Date</div>
-                            </ListGroupItemText>
-                        </ListGroupItem>                        
-                </ListGroup>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <Breadcrumb>
+                                <BreadcrumbItem>
+                                    <Link to="/">Search</Link>
+                                </BreadcrumbItem>
+                                <BreadcrumbItem>
+                                    <Link to={`/${this.props.match.params.username}/repositories`}>Repositories</Link>
+                                </BreadcrumbItem>
+                                <BreadcrumbItem active>
+                                    Commits
+                                </BreadcrumbItem>
+                            </Breadcrumb>                    
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <h3>Name of the repo</h3>
+                        </div>
+                        <div className="col-lg-4">
+                            <Input className="gh-search" placeholder="Enter text to search...." />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <ListGroup>
+
+                            </ListGroup>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
