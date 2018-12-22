59e9365574bcb79a989954a86fa66fdf8e2d7452
ibrahim-islam <ibrahim.islam@live.com>
2018-12-22 23:57:45 +0700

    implements searching commit message

--- src/components/commit/index.js
+++ src/components/commit/index.js
@@ -29,6 +29,13 @@ const CommitItem = ({headline, message, date}) => (
 
 export class Commit extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            commits: this.props.commitContainer.commits
+        }
+    }
+
     componentDidMount(){
         const {username, repo} = this.props.match.params
         if (this.props.commitContainer.repo.toLowerCase() !== repo) {
@@ -38,21 +45,37 @@ export class Commit extends Component {
         window.addEventListener('scroll', this._handleOnScroll)
     }
 
+    componentWillReceiveProps(newProps) {
+        this.setState({
+            commits: newProps.commitContainer.commits
+        })   
+    }
+
     componentWillUnmount() {
         window.removeEventListener('scroll', this._handleOnScroll)
     }
 
     _handleOnScroll = () => {
-        //TODO disable infinite scrolling if in search mode
-        //get this state prop from local state
-        if(this.props.showLoading || !this.props.commitContainer.pageInfo.hasNextPage) return
+        if(this.state.isSearching 
+            || this.props.showLoading 
+            || !this.props.commitContainer.pageInfo.hasNextPage) 
+            return
         
         if (window.innerHeight + document.documentElement.scrollTop
             === document.documentElement.offsetHeight)
             this.props.getCommits(this.props.match.params.username,
                 this.props.match.params.repo,
                 this.props.commitContainer.pageInfo.endCursor)
-        }
+    }
+
+    _onSearch = (event) => {
+        const searchTerm = event.currentTarget.value
+        this.setState({
+            isSearching: searchTerm.length > 0,
+            commits: this.props.commitContainer.commits
+                .filter(commit => commit.headline.toLowerCase().includes(searchTerm))
+        })
+    }
 
     render() {
         return (
@@ -68,7 +91,7 @@ export class Commit extends Component {
                                     { this.props.match.params.username }
                                 </BreadcrumbItem>
                                 <BreadcrumbItem>
-                                    <Link to="#" onClick={e => this.props.history.goBack()}>Repositories</Link>
+                                    <Link to="#" onClick={_ => this.props.history.goBack()}>Repositories</Link>
                                 </BreadcrumbItem>
                                 <BreadcrumbItem>
                                     { this.props.match.params.repo }
@@ -92,13 +115,13 @@ export class Commit extends Component {
                                     <h3>{this.props.match.params.repo}</h3>
                                 </div>
                                 <div className="col-lg-4">
-                                    <Input className="gh-search" placeholder="Enter text to search...." />
+                                    <Input onChange={this._onSearch} className="gh-search" placeholder="Enter text to search...." />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <ListGroup>
-                                        { this.props.commitContainer.commits.map(commit => <CommitItem key={commit.id} {...commit} />) }
+                                        { this.state.commits.map(commit => <CommitItem key={commit.id} {...commit} />) }
                                     </ListGroup>
                                 </div>
                             </div>
